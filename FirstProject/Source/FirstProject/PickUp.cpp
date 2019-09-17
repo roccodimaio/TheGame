583464e052a339344d85d16df08c2fd28e73b529
rdimaio <roccodimaio@yahoo.com>
2019-09-16 22:45:45 -0400

    Created C++ class called Item, created child classes from Item (Explosive and PickUp).  Enabled collision volumes and detect when OnOverlapBegin and OnOverlapEnd.  Out message to temp log on begin and end.

---
+++ FirstProject/Source/FirstProject/PickUp.cpp
@@ -0,0 +1,22 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PickUp.h"
+
+APickUp::APickUp()
+{
+
+}
+void APickUp::OnOverlapBegin(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	Super::OnOverlapBegin(OverlappedComponent, OtherActor, OtherComp, OtherBodyIndex, bFromSweep, SweepResult);
+
+	UE_LOG(LogTemp, Warning, TEXT("PickUp::OnOverLapBegin()"));
+}
+
+void APickUp::OnOverlapEnd(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
+	Super::OnOverlapEnd(OverlappedComponent, OtherActor, OtherComp, OtherBodyIndex);
+
+	UE_LOG(LogTemp, Warning, TEXT("PickUp::OnOverLapEnd()"));
+}
